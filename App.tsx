5fa4dcb55361e9d6cee8cabd0555d4b76bbafb6d
leoelkzaz <leoelkzaz@yahoo.com>
2022-02-28 01:14:07 +0200

    setting up fonts, icons and loading screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+// react-redux , redux is installed
+import { Ionicons } from "@expo/vector-icons";
+import { Provider } from "react-redux";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Cairo-Black": require("./fonts/Cairo-Black.ttf"),
+    "Cairo-Bold": require("./fonts/Cairo-Bold.ttf"),
+    "Cairo-ExtraBold": require("./fonts/Cairo-ExtraBold.ttf"),
+    "Cairo-Light": require("./fonts/Cairo-Light.ttf"),
+    "Cairo-Medium": require("./fonts/Cairo-Light.ttf"),
+    "Cairo-Regular": require("./fonts/Cairo-Regular.ttf"),
+    "Cairo-SemiBold": require("./fonts/Cairo-SemiBold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        // @ts-ignore
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.warn(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello Wheel.e!</Text>
+      <Ionicons name="heart" size={23} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 27,
+    fontFamily: "Cairo-Bold",
   },
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+## to use the mobile app frontend
+
+run npm start from the root directory
+install the expo app on mobile and scan the QR code that is provided in the terminal
