0528ba3455554d8776e382915f169a147127c38f
leoelkzaz <leoelkzaz@yahoo.com>
2022-03-10 13:38:42 +0200

    inputField component

---
+++ components/UI/InputField.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Platform,
+  Text,
+  TextInput,
+} from "react-native";
+import colors from "../../constants/colors";
+
+interface InputFieldProps {
+  placeHolder: string;
+  fieldStyle: object;
+  autoComplete: any;
+  value: string;
+  onChangeText: any;
+}
+
+const InputField = (props: InputFieldProps) => {
+  const { placeHolder, fieldStyle, autoComplete, value, onChangeText } = props;
+
+  return (
+    <TextInput
+      placeholder={placeHolder}
+      style={{ ...fieldStyle, ...styles.input }}
+      autoCompleteType={autoComplete}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+};
+
+export default InputField;
+
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 55,
+    backgroundColor: "#ccc",
+    textAlign: "center",
+  },
+  input: {
+    borderRadius: 15,
+    textAlign: "left",
+    padding: 20,
+    backgroundColor: colors.lightGray,
+  },
+});
