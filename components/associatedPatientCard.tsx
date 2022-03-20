5df7ff4587da0b117af42d883cf7f4234cf413a8
leoelkzaz <leoelkzaz@yahoo.com>
2022-03-20 23:10:19 +0200

    associated patient card component

---
+++ components/associatedPatientCard.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import colors from "../constants/colors";
+
+interface IAssociatedPatientProps {
+  patientName: string;
+  patientAddress: string;
+  onPress: () => void;
+  backgroundColor: string;
+}
+
+const AssociatedPatientCard = (props: IAssociatedPatientProps) => {
+  const { patientName, patientAddress, onPress, backgroundColor } = props;
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ ...styles.container, backgroundColor }}
+    >
+      <View>
+        <Text
+          style={
+            backgroundColor === colors.darkGreen
+              ? styles.whiteTitle
+              : styles.title
+          }
+        >
+          {patientName}
+        </Text>
+        <Text
+          style={
+            backgroundColor === colors.darkGreen
+              ? styles.whiteLocation
+              : styles.location
+          }
+        >
+          {patientAddress}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default AssociatedPatientCard;
+
+const styles = StyleSheet.create({
+  container: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    margin: 10,
+    padding: 10,
+    overflow: "hidden",
+    width: "90%",
+  },
+  title: {
+    fontFamily: "Cairo-Bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  whiteTitle: {
+    fontFamily: "Cairo-Bold",
+    fontSize: 20,
+    color: "#fff",
+  },
+  location: {
+    fontFamily: "Cairo-Medium",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  whiteLocation: {
+    fontFamily: "Cairo-Medium",
+    fontSize: 16,
+    color: "#fff",
+    textAlign: "center",
+  },
+});
