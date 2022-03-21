7a204f19528b6708c9a18f15dc0e737f525ee60b
leoelkzaz <leoelkzaz@yahoo.com>
2022-03-21 22:38:19 +0200

    associated patients screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import { HomeProps } from "../navigation/navigationUtils";
 import { MainButton } from "../components/UI/mainButton";
 import colors from "../constants/colors";
@@ -11,65 +11,71 @@ const HomeScreen = (props: HomeProps) => {
     <View style={styles.container}>
       <Text style={styles.title}>go to Profile</Text>
       <View style={styles.buttons}>
-        <MainButton
-          title="Supervisor Home Screen"
-          buttonStyle={{
-            backgroundColor: colors.darkGreen,
-            borderWidth: 2,
-            borderColor: "white",
-            borderRadius: 30,
-            justifyContent: "center",
-            padding: 15,
-            height: 150,
-            width: 200,
-            marginHorizontal: 50,
-            marginVertical: 10,
-          }}
-          titleStyle={{
-            fontFamily: "Cairo-Bold",
-            color: "white",
-            fontSize: 20,
-            textAlign: "center",
-          }}
-          onPress={() => props.navigation.navigate("SupervisorHome")}
-          icon={{ name: "ios-cart", size: 20, color: "white" }}
-          image={{ url: require("../assets/hospital.png") }}
-          hasIcon={false}
-        />
-        <MainButton
-          title="Patient Home Screen"
-          buttonStyle={{
-            backgroundColor: colors.darkPink,
-            borderWidth: 2,
-            borderColor: "white",
-            borderRadius: 30,
-            justifyContent: "center",
-            padding: 15,
-            height: 150,
-            width: 200,
-            marginHorizontal: 50,
-            marginVertical: 10,
-          }}
-          titleStyle={{
-            fontFamily: "Cairo-Bold",
-            color: "white",
-            fontSize: 20,
-            textAlign: "center",
-          }}
-          onPress={() => props.navigation.navigate("PatientHome")}
-          icon={{ name: "ios-cart", size: 20, color: "white" }}
-          image={{ url: require("../assets/hospital.png") }}
-          hasIcon={false}
-        />
-        <MainButton
-          title="Change language"
-          buttonStyle={styles.changeLanguageButton}
-          titleStyle={styles.changeLanguageText}
-          onPress={() => props.navigation.navigate("ChangeLanguage")}
-          icon={{ name: "globe-outline", size: 40, color: "black" }}
-          image={{ url: require("../assets/hospital.png") }}
-          hasIcon={true}
-        />
+        <ScrollView>
+          <MainButton
+            title="Supervisor Home Screen"
+            buttonStyle={{
+              backgroundColor: colors.darkGreen,
+              borderWidth: 2,
+              borderColor: "white",
+              borderRadius: 30,
+              justifyContent: "center",
+              padding: 15,
+              height: 150,
+              width: 200,
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            titleStyle={{
+              fontFamily: "Cairo-Bold",
+              color: "white",
+              fontSize: 20,
+              textAlign: "center",
+            }}
+            onPress={() => props.navigation.navigate("SupervisorHome")}
+            icon={{ name: "ios-cart", size: 20, color: "white" }}
+            image={{ url: require("../assets/hospital.png") }}
+            hasIcon={false}
+          />
+          <MainButton
+            title="Patient Home Screen"
+            buttonStyle={{
+              backgroundColor: colors.darkPink,
+              borderWidth: 2,
+              borderColor: "white",
+              borderRadius: 30,
+              justifyContent: "center",
+              padding: 15,
+              height: 150,
+              width: 200,
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            titleStyle={{
+              fontFamily: "Cairo-Bold",
+              color: "white",
+              fontSize: 20,
+              textAlign: "center",
+            }}
+            onPress={() => props.navigation.navigate("PatientHome")}
+            icon={{ name: "ios-cart", size: 20, color: "white" }}
+            image={{ url: require("../assets/hospital.png") }}
+            hasIcon={false}
+          />
+          <MainButton
+            title="Change language"
+            buttonStyle={styles.changeLanguageButton}
+            titleStyle={styles.changeLanguageText}
+            onPress={() => props.navigation.navigate("ChangeLanguage")}
+            icon={{ name: "globe-outline", size: 40, color: "black" }}
+            image={{ url: require("../assets/hospital.png") }}
+            hasIcon={true}
+          />
+          <Button
+            title="GO TO Associated Patients"
+            onPress={() => props.navigation.navigate("AssociatedPatients")}
+          />
+        </ScrollView>
       </View>
     </View>
   );
@@ -87,7 +93,7 @@ const styles = StyleSheet.create({
     fontFamily: "Cairo-Bold",
   },
   buttons: {
-    margin: 10,
+    marginBottom: 70,
     width: 300,
   },
   changeLanguageButton: {
