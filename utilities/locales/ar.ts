874588524316ec74479d007d63bb8de3cba02ceb
Rania-Daoud <rania.a.dawod@gmail.com>
2022-03-26 00:22:26 +0200

    Add signInScreen component

--- utilities/locales/ar.ts
+++ utilities/locales/ar.ts
@@ -7,5 +7,12 @@ export default {
     healthInfo: "السجلات الصحية",
     freeDrive: "القيادة بحرية",
     freeDriveText: "استخدم الأسهم للحركة في أي اتجاه",
+    WelcomeBack: "مرحبًا بك",
+    emailAddress: "البريد الإلكتروني",
+    password: "رمز الدخول",
+    logIn: "تسجيل الدخول",
+    forgotPassword: "نسيت رمز الدخول؟",
+    notMember: "لا تمتلك حساب؟",
+    signUp: "أنشيٍ حساب",
   },
 };
--- utilities/locales/en.ts
+++ utilities/locales/en.ts
@@ -7,5 +7,12 @@ export default {
     healthInfo: "Health Info",
     freeDrive: "Free Drive",
     freeDriveText: "Use the cursor to move the chair around.",
+    WelcomeBack: "Welcome Back!",
+    emailAddress: "Email Address",
+    password: "Password",
+    logIn: "LOG IN",
+    forgotPassword: "Forgot Password?",
+    notMember: "Not a member?",
+    signUp: "sign up",
   },
 };
