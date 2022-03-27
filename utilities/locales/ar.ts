bf43e91de8d786f4bb0202bb02cd48b3e93db8ba
leoelkzaz <leoelkzaz@yahoo.com>
2022-03-27 02:17:52 +0200

    Refactoring and adding translation

--- utilities/locales/ar.ts
+++ utilities/locales/ar.ts
@@ -7,5 +7,10 @@ export default {
     healthInfo: "السجلات الصحية",
     freeDrive: "القيادة بحرية",
     freeDriveText: "استخدم الأسهم للحركة في أي اتجاه",
+    newPatient: "أضف مريضًا جديدًا",
+    patientsStatus: "حالات المرضي",
+    help: "المساعدة",
+    wishGoodDay: "نتمني لك يومًا سعيدًا!",
+    morning: "صباح الخير, ",
   },
 };
