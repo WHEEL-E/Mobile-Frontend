9625ac2248db9804e79c4ec99a80e0e588b990c1
Rania-Daoud <rania.a.dawod@gmail.com>
2022-07-01 14:25:57 +0200

    Add screen implementation

---
+++ components/healthStatusComponents/SensorCard.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import colors from "../../utilities/constants/colors";
+import {
+  DEVICE_HEIGHT,
+  DEVICE_WIDTH,
+} from "../../utilities/constants/dimentions";
+import { BIG_MARGIN_VERTICAL } from "../../utilities/constants/spacing";
+import { ImportantText, TitleText } from "../../utilities/types/fontTypes";
+import { SensorCardProps } from "../../utilities/types/healthStatusTypes";
+
+export const SensorCard = (props: SensorCardProps) => {
+  const {
+    sensor: { image, sensorName, value, unit },
+  } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle}>
+        <Image source={image} style={styles.image} resizeMode="center" />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{sensorName}</Text>
+        <Text style={styles.value}>
+          {value} {unit}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: DEVICE_HEIGHT * 0.15,
+    width: "90%",
+    flexDirection: "row",
+    marginVertical: BIG_MARGIN_VERTICAL,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  circle: {
+    width: DEVICE_WIDTH * 0.3,
+    height: DEVICE_WIDTH * 0.3,
+    borderRadius: DEVICE_WIDTH * 0.2,
+    backgroundColor: colors.lightPurple,
+    borderColor: colors.darkPurple,
+    borderWidth: DEVICE_WIDTH * 0.01,
+    shadowOffset: { width: 3, height: 3 },
+    shadowOpacity: 0.8,
+    shadowColor: "black",
+    shadowRadius: DEVICE_WIDTH * 0.2,
+    elevation: DEVICE_HEIGHT * 0.015,
+    overflow: "hidden",
+  },
+  image: { width: "100%", height: "100%" },
+  textContainer: {
+    width: DEVICE_WIDTH * 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: { ...ImportantText },
+  value: { ...TitleText },
+});
--- screens/HealthStatusScreen.tsx
+++ screens/HealthStatusScreen.tsx
@@ -1,11 +1,76 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { Socket } from "socket.io-client";
+import { SensorCard } from "../components/healthStatusComponents/SensorCard";
+// import { startConnection } from "../store/actions/healthMonitoring";
+import { RootState } from "../store/reducers/rootReducer";
+import { ImportantNote, NormalText } from "../utilities/types/fontTypes";
 import { HealthStatusScreenProps } from "../utilities/types/navigationTypes/mainNavigationTypes";
 
 const HealthStatusScreen = (props: HealthStatusScreenProps) => {
+  const dispatch = useDispatch<any>();
+  const [sensorsData, setSensorsData] = React.useState({
+    SPO2: 0,
+    user_id: 1,
+    Pulse: 0,
+    time: "19:33:14",
+  });
+
+  //   const socket: Socket | undefined = useSelector(
+  //     (state: RootState) => state.healthMonitoring.socket
+  //   );
+
+  //   React.useEffect(() => {
+  //     dispatch(startConnection());
+  //   }, []);
+
+  //   if (socket) {
+  //     socket.on("data", (message) => {
+  //       setSensorsData(message);
+  //     });
+  //   }
+
+  const sensorsValues = [
+    {
+      sensorName: "Oxygen Saturation",
+      image: require("../assets/images/bloodDrop.png"),
+      value: sensorsData.SPO2,
+      unit: "mm Hg",
+    },
+    {
+      sensorName: "Heart Rate",
+      image: require("../assets/images/heart.png"),
+      value: sensorsData.Pulse,
+      unit: "BPM",
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>HealthStatus</Text>
+      <ImageBackground
+        source={require("../assets/images/Vector.png")}
+        style={styles.backgroundImage}
+      >
+        <View style={{}}>
+          {sensorsValues.map((item, index) => (
+            <SensorCard sensor={item} key={index} />
+          ))}
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.note}>
+            To get updated values please place your hands at the sensors placed
+            by your chair
+          </Text>
+          <Text style={ImportantNote}>Last Updated at: {sensorsData.time}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -16,6 +81,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    resizeMode: "cover",
+    paddingTop: "40%",
+    paddingBottom: "20%",
+  },
+  note: {
+    ...NormalText,
+    textAlign: "center",
+    paddingHorizontal: "5%",
+    paddingVertical: "5%",
   },
 });
 
